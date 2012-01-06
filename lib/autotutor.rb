3018986e77332a2d6a22c1df7710f51ccd586872
John Tjanaka <zonecheung@gmail.com>
2012-01-06 04:34:58 -0500

    Use module instead of class for Autotutor::Base.

--- lib/autotutor.rb
+++ lib/autotutor.rb
@@ -1,10 +1,23 @@
 require "capybara"
 
 module Autotutor
-  class Base
-    def task(*args, &block)
-      print 'abc'
-      yield
+  module Base
+
+    module ClassMethods
+    end
+
+    module InstanceMethods
+      def task(*args, &block)
+        print 'abc'
+        yield
+      end
+    end
+
+    def self.included(klass)
+      klass.module_eval {
+        extend ClassMethods
+        include InstanceMethods
+      }
     end
   end
 end
